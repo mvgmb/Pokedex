e36b29425c64112b34e78b37efbc27390a1d4c0e
mario <mvgmb@cin.ufpe.br>
2016-11-21 19:54:16 -0300

    coach, gym leader and trainer

---
+++ src/basico/Pokemon.java
@@ -0,0 +1,31 @@
+package basico;
+
+public class Pokemon {
+    private int number;
+    private String name;
+    private String type;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
