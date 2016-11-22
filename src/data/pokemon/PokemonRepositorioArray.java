ee9a8e554adff2dd967ebd6e9d1d085a7b2cf17e
mlbas <mlbas@cin.ufpe.br>
2016-11-22 09:08:29 -0300

    lul

---
+++ src/data/pokemon/PokemonRepositorioArray.java
@@ -0,0 +1,50 @@
+package data.pokemon;
+import classesprimarias.*;
+public class PokemonRepositorioArray
+{
+    private Pokemon[] array = new Pokemon[0];
+
+    public void insert(Pokemon pokemon)
+    {
+        Pokemon[] aux = new Pokemon[array.length + 1];
+        for (int i = 0; i < array.length; i++)
+            aux[i] = array[i];
+        aux[array.length] = pokemon;
+        array = aux;
+    }
+
+    public void remove(Pokemon pokemon)
+    {
+        Pokemon[] aux = new Pokemon[array.length - 1];
+        for (int i = 0; i < array.length; i++)
+            if (array[i].getNumber() == pokemon.getNumber())
+                array[i] = array[array.length - 1];
+        for (int i = 0; i < (array.length - 1); i++)
+            aux[i] = array[i];
+        this.array = aux;
+    }
+
+    public void update(Pokemon pokemon)
+    {
+        for (int i = 0; i < array.length; i++)
+            if (array[i].getNumber() == pokemon.getNumber())
+            {
+                array[i] = pokemon;
+                return;
+            }
+    }
+    public Pokemon search(Pokemon pokemon)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i].getNumber() == pokemon.getNumber())
+                return array[i];
+        }
+        return null;
+    }
+    public boolean findet(Pokemon pokemon)
+    {
+        return search(pokemon) != null;
+    }
+
+}
